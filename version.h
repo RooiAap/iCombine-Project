cbd0a5dbf953ef5e4190a2344ab5bee6d93a3f8a
Brendan Oelofse <bren.oelofse@gmail.com>
2023-04-26 16:55:01 +0200

    Updated version info

--- version.h
+++ version.h
@@ -5,13 +5,14 @@
 #include <QVersionNumber>
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 1
-#define VERSION_PATCH 2
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
 
 // 1.0.1: Added user input validation
 // 1.1.0: Added test grouping for summary data
 // 1.1.1: Added summary data
 // 1.1.2: Fixed test pass criteria
+// 1.2.0: Added file inspection functionality
 
 QVersionNumber version(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 
