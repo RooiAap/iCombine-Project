bb36b82bf0c19fdb1f2059c5819fa475b3dffd9b
Brendan Oelofse <bren.oelofse@gmail.com>
2023-05-03 12:41:31 +0200

    Updated version information

--- version.h
+++ version.h
@@ -6,7 +6,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 3
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 
 // 1.0.1: Added user input validation
 // 1.1.0: Added test grouping for summary data
@@ -14,6 +14,7 @@
 // 1.1.2: Fixed test pass criteria
 // 1.2.0: Added file inspection functionality
 // 1.3.0: Added export to csv functionality
+// 1.3.1: Minor UI fixes
 
 QVersionNumber version(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 
